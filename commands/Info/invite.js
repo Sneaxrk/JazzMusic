68fa074b4528100b7890f40b5101e4ef0f08fcb9
PEACExM <102233501+PEACExM@users.noreply.github.com>
2022-08-04 12:29:12 +0530

    Update invite.js

--- commands/Info/invite.js
+++ commands/Info/invite.js
@@ -28,10 +28,6 @@ let aboutembed = new MessageEmbed()
   
    [Click Here](https://discord.gg/fdasM6ez3u)
 
-<:upvote:994973001894338670> **__Vote__** : 
-  
-   [Click Here](https://top.gg/bot/998956313969164339/vote)
-
 `)
 
 
@@ -48,15 +44,9 @@ const button1 = new MessageButton()
   .setURL("https://discord.gg/fdasM6ez3u")
   .setStyle(`LINK`)
         .setEmoji(`994973299840921650`)     
-
-  const button3 = new MessageButton()
-  .setLabel(`Vote`)
-  .setURL("https://top.gg/bot/998956313969164339/vote")
-  .setStyle(`LINK`)
-        .setEmoji(`994973001894338670`)       
       //.setEmoji(`896959039999717398`)
   const row = new MessageActionRow()
-  .addComponents(button1, button2, button3) 	  
+  .addComponents(button1, button2) 	  
 message.channel.send({embeds: [aboutembed], components: [row]});
       
   //      message.channel.send({ embeds: [aboutembed], components : [row] })
